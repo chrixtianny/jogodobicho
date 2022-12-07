454990321ebc7727c5bca819b5d378dcaf28fb95
chrixtianny <102628110+chrixtianny@users.noreply.github.com>
2022-12-07 10:25:40 -0300

    Código base para sorteio das dezenas.py

--- bicho.py
+++ bicho.py
@@ -1,72 +1,45 @@
 import random
 import numpy as np
 
-NumeroDoBicho = np.random.randint(1, 51, (2))
-
-print('As dezenas sorteadas são', NumeroDoBicho[0],'e', NumeroDoBicho[1])
-
-#Primeira Dezena
-
-if NumeroDoBicho[0] > 0 and NumeroDoBicho[0] < 6:
-  print (NumeroDoBicho[0], "O bicho é o Major Porco")
-
-elif NumeroDoBicho[0] > 5 and NumeroDoBicho[0] < 11: 
-  print(NumeroDoBicho[0], "O bicho é o Porco Bola de Neve")
-
-elif NumeroDoBicho[0] > 10 and NumeroDoBicho[0] < 16:
- print(NumeroDoBicho[0], "O bicho é o Porco Napoleão")
-
-elif NumeroDoBicho[0] > 15 and NumeroDoBicho[0] < 21:
- print(NumeroDoBicho[0], "Os bichos são Ovelhas")
-
-elif NumeroDoBicho[0] > 20 and NumeroDoBicho[0] < 26:
- print(NumeroDoBicho[0], "O bicho é Benjamin, o Burro")
-
-elif NumeroDoBicho[0] > 24 and NumeroDoBicho[0] < 31:
- print(NumeroDoBicho[0], "O bicho é Sansão, o Cavalo")
-
-elif NumeroDoBicho[0] > 30 and NumeroDoBicho[0] < 36:
- print(NumeroDoBicho[0], "O bicho é Maricota, a Cabra")
-
-elif NumeroDoBicho[0] > 35 and NumeroDoBicho[0] < 41:
- print(NumeroDoBicho[0], "Os bichos são os Cachorros")
-
-elif NumeroDoBicho[0] > 40 and NumeroDoBicho[0] < 46:
- print(NumeroDoBicho[0], "O bicho é Mimosa, a Égua")
-
-elif NumeroDoBicho[0] > 45 and NumeroDoBicho[0] < 51:
-
-print(NumeroDoBicho[0], "O bicho é Quitéria, a Égua")
-
-  #Segunda Dezena
-if NumeroDoBicho[1] > 0 and NumeroDoBicho[1] < 6:
-  print (NumeroDoBicho[1], "O bicho é o Major Porco")
+NumeroDoBicho = np.random.randint(1, 56, (5))
+x = 0
+while x < 5:
+  print('Dezena sorteada:', NumeroDoBicho[x])
   
-elif NumeroDoBicho[1] > 5 and NumeroDoBicho[1] < 11: 
-  print(NumeroDoBicho[1], "O bicho é o Porco Bola de Neve")
-
-elif NumeroDoBicho[1] > 10 and NumeroDoBicho[1] < 16:
- print(NumeroDoBicho[1], "O bicho é o Porco Napoleão")
-
-elif NumeroDoBicho[1] > 15 and NumeroDoBicho[1] < 21:
- print(NumeroDoBicho[1], "Os bichos são Ovelhas")
-
-elif NumeroDoBicho[1] > 20 and NumeroDoBicho[1] < 26:
- print(NumeroDoBicho[1], "O bicho é Benjamin, o Burro")
-
-elif NumeroDoBicho[1] > 24 and NumeroDoBicho[1] < 31:
- print(NumeroDoBicho[1], "O bicho é Sansão, o Cavalo")
-
-elif NumeroDoBicho[1] > 30 and NumeroDoBicho[1] < 36:
- print(NumeroDoBicho[1], "O bicho é Maricota, a Cabra")
-
-elif NumeroDoBicho[1] > 35 and NumeroDoBicho[1] < 41:
- print(NumeroDoBicho[1], "Os bichos são os Cachorros")
+  if NumeroDoBicho[x] > 0 and NumeroDoBicho[x] < 6:
+    print (NumeroDoBicho[x], "O bicho é o Major Porco")
   
-elif NumeroDoBicho[1] > 40 and NumeroDoBicho[1] < 46:
+  elif NumeroDoBicho[x] > 5 and NumeroDoBicho[x] < 11: 
+    print(NumeroDoBicho[x], "O bicho é o Porco Bola de Neve")
+  
+  elif NumeroDoBicho[x] > 10 and NumeroDoBicho[x] < 16:
+   print(NumeroDoBicho[x], "O bicho é o Porco Napoleão")
+  
+  elif NumeroDoBicho[x] > 15 and NumeroDoBicho[x] < 21:
+   print(NumeroDoBicho[x], "Os bichos são Ovelhas")
+  
+  elif NumeroDoBicho[x] > 20 and NumeroDoBicho[x] < 26:
+   print(NumeroDoBicho[x], "O bicho é Benjamin, o Burro")
+  
+  elif NumeroDoBicho[x] > 24 and NumeroDoBicho[x] < 31:
+   print(NumeroDoBicho[x], "O bicho é Sansão, o Cavalo")
+  
+  elif NumeroDoBicho[x] > 30 and NumeroDoBicho[x] < 36:
+   print(NumeroDoBicho[x], "O bicho é Maricota, a Cabra")
+  
+  elif NumeroDoBicho[x] > 35 and NumeroDoBicho[x] < 41:
+   print(NumeroDoBicho[x], "Os bichos são os Cachorros")
+  
+  elif NumeroDoBicho[x] > 40 and NumeroDoBicho[x] < 46:
+   print(NumeroDoBicho[x], "O bicho é Mimosa, a Égua")
+  
+  elif NumeroDoBicho[x] > 45 and NumeroDoBicho[x] < 51:
+  
+   print(NumeroDoBicho[x], "O bicho é Quitéria, a Égua")
+  
+  elif NumeroDoBicho[x] > 50 and NumeroDoBicho[x] < 56:
+  
+    print(NumeroDoBicho[x], "O bicho é Moisés, o Corvo")
 
- print(NumeroDoBicho[1], "O bicho é Mimosa, a Égua")
-
-elif NumeroDoBicho[1] > 45 and NumeroDoBicho[1] < 51:
-
- print(NumeroDoBicho[1], "O bicho é Quitéria, a Égua")
+  
+  x = x + 1
